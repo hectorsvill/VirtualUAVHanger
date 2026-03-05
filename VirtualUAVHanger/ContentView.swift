6413365a3ada86f17dcad2e5feb7bf0e8f7b197a
“hectorsvill” <hectorstevenvillasano@gmail.com>
2026-03-05 16:58:27 -0500

    Initial Commit

---
+++ VirtualUAVHanger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VirtualUAVHanger
+//
+//  Created by Hector Steven Villasano on 3/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VirtualUAVHanger/VirtualUAVHangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualUAVHangerApp.swift
+//  VirtualUAVHanger
+//
+//  Created by Hector Steven Villasano on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct VirtualUAVHangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
